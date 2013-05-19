a79bd8b46ef9e943817109433f750f2f053f6c94
Lucas Cimon <lucas.cimon@gmail.com>
2013-05-19 13:20:53 +0100

    Python notes cleaning

--- python/notes.py
+++ python/notes.py
@@ -2,24 +2,14 @@
 "" Python
 """""""""""
 
-# AWESOME for scripting
-http://amoffat.github.io/sh/
-
-# HTTP server
-python -m SimpleHTTPServer 8080 # --version > 3: -m http.server
-
 __slots__ = ("attr1_name")  # attribute
 # Its proper use is "to save space in objects. Instead of having a dynamic dict that allows adding attributes to objects at anytime, there is a static structure which does not allow additions after creation. This saves the overhead of one dict for every object that uses slots."
 
-# get exec time
-python -mtimeit -s'xs=range(10)' '[hex(x) for x in xs]' # or 'map(hex, xs)'
-
 # DO NOT use other default parameter values than None
+# + initialization is static
 def foo(x = []):
     x.append('do')
     return x
-# + initialization is static
-
 foo()
 foo()
 
@@ -40,17 +30,15 @@ __all__ = ['bar', 'foo']
 __call__
 # if a = A(), this is the method called when doing a()
 
-Foo = type('Foo', (object,), {'bar':True})
+globals()["Foo"] = Foo = type('Foo', (object,), {'bar':True})
 # on-the-fly class creation
+# Cool alternative for Nose tests, as TestCase and generators are not compatible
 
 # 'type' is the metaclass Python uses to create all classes behind the scenes
 # aka, the most common __class__.__class__ of an object
 # But you can specify your own __metaclass__ !
 
-# PyCharm : code inspection
-
-# http://stackoverflow.com/questions/132058/showing-the-stack-trace-from-a-running-python-application
-
+# Functions attributes
 def foo(n):
     def inner(i):
         inner.n += i
@@ -58,14 +46,7 @@ def foo(n):
     inner.n = n
     return inner
 
-# GOTCHAS:
-#- http://code.activestate.com/recipes/502271-these-nasty-closures-caveats-for-the-closure-enthu/
-#- http://stackoverflow.com/questions/12182068/python-closure-function-losing-outer-variable-access
-
-# In Nose, TestCase and generators are not compatible : here is an alternative
-globals()[test_class_name] = type(test_class_name, (ApolloEnvironmentTest,TestCase), {"attr":attr})
-
-# decorator with args
+# Decorator with args
 def my_decorator(decorator_args):
     def tmp_decorator(orig_func):
         new_func = orig_func
@@ -83,6 +64,31 @@ class Dict(dict):
     def __setattr__(self, name, value):
         self[name] = value
 
+@patch("module.CONSTANT", new_value)
+def foo(): ...
+
+code = "my code bla bla"
+compiled = compile(code)
+exec compiled 
+
+# Signal-based handle on a program to debug
+# http://stackoverflow.com/questions/132058/showing-the-stack-trace-from-a-running-python-application
+
+# GOTCHAS:
+#- http://code.activestate.com/recipes/502271-these-nasty-closures-caveats-for-the-closure-enthu/
+#- http://stackoverflow.com/questions/12182068/python-closure-function-losing-outer-variable-access
+
+# HTTP server
+python -m SimpleHTTPServer 8080 # --version > 3: -m http.server
+
+# AWESOME for scripting
+http://amoffat.github.io/sh/
+
+# PyCharm : code inspection
+
+# get exec time
+python -mtimeit -s'xs=range(10)' '[hex(x) for x in xs]' # or 'map(hex, xs)'
+
 # Built-in profiler
 python -m cProfile myscript.py
 # Visu:
@@ -91,9 +97,3 @@ https://tech.dropbox.com/2012/07/plop-low-overhead-profiling-for-python/
 # And also
 http://mg.pov.lt/objgraph/
 
-@patch("module.CONSTANT", new_value)
-def foo(): ...
-
-code = "my code bla bla"
-compiled = compile(code)
-exec compiled 
