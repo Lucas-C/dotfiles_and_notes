54e656a03e378ff61267defea38868c48d1b94c4
Lucas Cimon <lucas.cimon@gmail.com>
2013-08-14 17:42:23 +0200

    A new attempt to Python frozendict

--- python/immutable.py
+++ python/immutable.py
@@ -76,62 +76,48 @@ def _dict2NamedTuple(d):
 
 # !WARNING! frozendcit are not pythonic, see: http://www.python.org/dev/peps/pep-0416/
 
-# FROM: http://code.activestate.com/recipes/576540/
-# PROS:
-#   builtin
-# CONS:
-#   4 parasite keys : ['__dict__', '__module__', '__weakref__', '__doc__']
-#   not isinstance(di, dict)
-"""
-make_dictproxy = lambda dictobj: type('',(),dictobj).__dict__
-"""
-
-# Based on an idea from ThibT: closure poweeer !
 # 'Whitelist' approach
-# Could be made hashable
+# Credits :
+#   Thibault Toledano for the original closure idea
+#   http://code.activestate.com/recipes/576540/
+# PROS
+#   really read-only as values in closures cannot be re-assigned (see below)
+#   use a builtin data structure
+#   dict(make_frozendict(a_dict)) == a_dict
+#   the object passed as a parameter doesn't need to be a dict, just to support dict "cast", and an additionnal copy will then be made
+#       e.g. make_frozendict(make_frozendict(a_dict))
 # CONS
-#   not isinstance(make_frozendict({}), dict)
+#   4 blacklisted keys
 #   make_frozendict({}).__class__ != make_frozendict({}).__class__
-#   NOT REALLY read-only : get_cell_value(di.__getitem__.im_func.func_closure[0]) give access to the mutable dict
-"""
+#   not isinstance(make_frozendict({}), dict)
+# EXTENSIONS
+#   could be made hashable
+#   could implement items/iteritems more efficiently (collections.Mapping return (key, self[key]), which cost a lookup each time)
+
+_DICTPROXY_EXTRA_KEYS = tuple(type('',(),{}).__dict__.keys())
 def make_frozendict(original_dict):
-    dict_copy = original_dict.copy()
+    if not isinstance(original_dict, dict):
+        original_dict = dict(original_dict)
+    for key in _DICTPROXY_EXTRA_KEYS:
+        if key in original_dict:
+            raise ValueError("frozendict cannot contain '{}' as a key (blacklisted keys : {})".format(key, _DICTPROXY_EXTRA_KEYS)) 
+    dict_proxy = type('',(),original_dict).__dict__
     class frozendict(Mapping):
         def __getitem__(self, key):
-            return dict_copy[key]
+            if key in _DICTPROXY_EXTRA_KEYS:
+                raise KeyError(key)
+            return dict_proxy[key]
         def __iter__(self):
-            return iter(dict_copy)
+            return (k for k in dict_proxy if k not in _DICTPROXY_EXTRA_KEYS)
         def __len__(self):
-            return len(dict_copy)
+            return len(dict_proxy) - len(_DICTPROXY_EXTRA_KEYS)
         def __repr__(self):
-            return "frozendict({!r})".format(dict_copy)
+            return "make_frozendict({!r})".format({k:self[k] for k in self})
     return frozendict()
-"""
 
-# FROM: http://code.activestate.com/recipes/414283-frozen-dictionaries/
-# 'Blacklist' approach
-"""
-class frozendict(dict):
-    def __new__(cls, *args, **kwargs):
-        new = dict.__new__(cls)
-        dict.__init__(new, *args, **kwargs)
-        new._cached_hash = hash(tuple(sorted(new.items())))
-        return new
 
-    def __init__(self, *args, **kwargs):
-        pass # could be used to modify the frozen dictionary ; ignored because it will be called once on initialization
-
-    def _blocked_attribute(obj):
-        raise AttributeError, "A frozendict cannot be modified."
-    _blocked_attribute = property(_blocked_attribute)
-
-    __delitem__ = __setitem__ = clear = _blocked_attribute
-    pop = popitem = setdefault = update = _blocked_attribute
-
-    def __hash__(self):
-        return self._cached_hash
-
-    def __repr__(self):
-        return "frozendict({})".format(dict.__repr__(self))
-"""
+# Closure are not writable:
+#   foo.func_closure = None # TypeError: readonly attribute
+#   foo.func_closure[0] = None # TypeError: 'tuple' object does not support item assignment
+#   foo.func_closure[0].cell_contents = None # AttributeError: attribute 'cell_contents' of 'cell' objects is not writable
 
