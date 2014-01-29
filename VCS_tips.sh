a73868a34e6c2549d8f0a8a18181b97c6def41a5
Lucas Cimon <lucas.cimon@gmail.com>
2014-01-29 00:06:20 +0000

    Julia notes

--- VCS_tips.sh
+++ VCS_tips.sh
@@ -24,6 +24,9 @@ svn diff | diffstat # sum-up a diff
 #*****#
 # http://blog.jacius.info/2008/6/22/git-tip-fix-a-mistake-in-a-previous-commit/
 
+# use HTTPS protocol instead of git one (e.g. to bypass a firefall)
+git config --global url."https://".insteadOf git://
+
 # Git 'uncommit', as 'don't-change-any-files-but-cancel-last-commit'
 git reset HEAD^
 
--- julia.jl
+++ julia.jl
@@ -1,3 +1,27 @@
 f(x) = x * x
 code_llvm(f, (Float64,))
 code_native(f, (Float64,))
+
+# 1-based indexing
+ismatch(r"^s*#.*$", "# a comment")
+
+h = (x, y) -> x + y
++(1, 2, 3) # operators are functions
+fib(n::Int) = n < 2 ? 1 : fib(n - 2) + fib(n - 1) # optional type annotations
+
+type Person
+    height::Real
+    weight::Real
+end
+
+# Matrices
+a = [1 2 3; 4 5 6] # 2x3 array
+[i * j for i=1:10, j=1:10] # 10x10
+b = eye(10, 10) * randn(10, 10)
+
+d = {"name" => "Jack", "id" => 123}
+
+Pkg.available() # return an array of all packages available
+Pkg.add("JSON")
+using JSON
+json(d)
