3d0f367b1af312e17cd536fb1ce2a69fe2552da0
Lucas Cimon <lucas.cimon@gmail.com>
2014-05-20 13:07:53 +0100

    Cleaning up pdb_on_err.py - Now it just needs some docstrings...

--- languages/python/pdb_on_err.py
+++ languages/python/pdb_on_err.py
@@ -7,50 +7,57 @@
 #   import pdb_on_err # in a script.py
 # FROM: http://stackoverflow.com/questions/242485/starting-python-debugger-automatically-on-error
 from imp import find_module
-from os import path
 import sys
-import pdb, traceback
+try:
+    from ipdb import launch_ipdb_on_exception as launch_pdb_on_exception
+except ImportError:
+    import contextlib
+    import pdb, traceback
 
-def excepthook(type, value, tb):
-    # do nothing if we are in interactive mode or we don't have a tty-like device
-    if not hasattr(sys, 'ps1') and sys.stderr.isatty():
-        # print the exception...
-        traceback.print_exception(type, value, tb)
-        print
-        # ...then start the debugger in post-mortem mode.
-        pdb.post_mortem(tb)
-    sys.__excepthook__(type, value, tb)
+    @contextlib.contextmanager
+    def launch_pdb_on_exception():
+        ps1_before = getattr(sys, 'ps1', None)
+        if ps1_before:
+            delattr(sys, 'ps1')
+        excepthook_before = sys.excepthook
+        sys.excepthook = pdb_excepthook
+        yield
+        sys.excepthook = excepthook_before
+        if ps1_before:
+            setattr(sys, 'ps1', ps1_before)
 
-def next_arg_is(flag):
-    try:
-        c_flag_index = sys.argv.index(flag)
-        del sys.argv[c_flag_index]
-        arg = sys.argv[c_flag_index]
-        del sys.argv[c_flag_index]
-        return arg
-    except ValueError:
-        pass
+    def pdb_excepthook(type, value, tb):
+        # do nothing if we are in interactive mode or we don't have a tty-like device
+        if not hasattr(sys, 'ps1') and sys.stderr.isatty():
+            # print the exception...
+            traceback.print_exception(type, value, tb)
+            print
+            # ...then start the debugger in post-mortem mode.
+            pdb.post_mortem(tb)
+        sys.__excepthook__(type, value, tb)
 
 def main():
     sys.argv = sys.argv[1:]
-    cmdstr = next_arg_is('-c')
+    cmdstr = get_arg_value_if_next(sys.argv, '-c')
     if cmdstr:
         exec(cmdstr)
         return
-    modname = next_arg_is('-m')
+    modname = get_arg_value_if_next(sys.argv, '-m')
     if modname:
         filename = find_module(modname)[1]
     else:
         filename = sys.argv[0]
     execfile(filename, {'__name__':'__main__', '__file__':filename})
 
-if __name__ == '__main__':
+def get_arg_value_if_next(argv, flag):
     try:
-        from ipdb import launch_ipdb_on_exception
-        with launch_ipdb_on_exception():
-            main()
-    except ImportError:
-        if hasattr(sys, 'ps1'):
-            delattr(sys, 'ps1')
-        sys.excepthook = excepthook
+        c_flag_index = argv.index(flag)
+    except ValueError:
+        return
+    del argv[c_flag_index]
+    return argv.pop(c_flag_index)
+
+if __name__ == '__main__':
+    with launch_pdb_on_exception():
         main()
+
