e7c79d3909d327029bfa729b93ad17c39cd7c908
Lucas Cimon <lucas.cimon@gmail.com>
2014-09-09 16:52:12 +0200

    Adding languages/python/html_dir_crawler.py

---
+++ languages/python/html_dir_crawler.py
@@ -0,0 +1,36 @@
+from collections import Counter, OrderedDict
+import json
+import os
+import scrapy
+import urlparse
+
+class FileUrl(scrapy.Item):
+    url = scrapy.Field()
+
+class HtmlDirectoryCrawler(scrapy.Spider):
+    name = os.path.basename(__file__)
+    def __init__(self, url=''):
+        self.start_urls = [url]
+        self.ext_counter = Counter()
+    def parse(self, response):
+        for href in response.xpath('//a/@href')[5:]:
+        # Skipping the 5 first hrefs: Name, Last modified, Size, Description, Parent Folder
+            child_url = urlparse.urljoin(response.url, href.extract())
+            is_garbage = child_url.endswith('_fichiers/')
+            if is_garbage:
+                continue
+            is_folder = (child_url[-1] == '/')
+            if is_folder:
+                yield scrapy.Request(child_url, self.parse)
+            else:
+                ext = child_url.split('.')[-1]
+                self.ext_counter[ext] += 1
+                yield FileUrl(url=child_url)
+    def closed(self, reason):
+        ordered_ext_counter = OrderedDict(sorted(
+                self.ext_counter.iteritems(),
+                key=lambda (k,v): (v,k)))
+        self.log("Stats on extensions found:\n{}".format(
+            json.dumps(ordered_ext_counter, indent=4)),
+            level=scrapy.log.INFO)
+
