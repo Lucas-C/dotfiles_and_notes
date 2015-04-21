27646c142f398a7beaa7bc25318613461d9f2948
Lucas Cimon <lucas.cimon@gmail.com>
2015-04-21 15:41:07 +0200

    Various Windows-based tips & fixes

--- misc/installCygwin.sh
+++ misc/installCygwin.sh
@@ -14,7 +14,13 @@ lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
 sed -i 's/md5sum/sha512sum/g' apt-cyg
 install apt-cyg ~/bin
 apt-cyg install wget
-apt-cyg install git
+apt-cyg install bind curl git rlwrap unzip vim
+
+wget https://bootstrap.pypa.io/ez_setup.py -O - | python
+easy_install pip
+
+apt-cyg install gcc-g++ libffi-devel libxml2-devel libxslt-devel openssl-devel
+pip install --user service_identity scrapy
 
 cd /
 [ -e /c ] || ln -s /cygdrive/c
