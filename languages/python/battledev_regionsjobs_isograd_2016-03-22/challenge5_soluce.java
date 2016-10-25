d973d7a20d6019d7c83465c194aed583bfc8a9cc
Lucas Cimon <lucas.cimon@gmail.com>
2016-10-25 13:24:55 +0200

    Ajout challenge5_soluce.java

---
+++ languages/python/battledev_regionsjobs_isograd_2016-03-22/challenge5_soluce.java
@@ -0,0 +1,42 @@
+
+/*
+** SOLUTION by Zeldarck
+*/
+package com.isograd.exercise;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IsoContest {
+  public static void main( String[] argv ) throws Exception {
+    String  line;
+    Scanner sc = new Scanner(System.in);
+    String[] tab = null;
+    long res =0;
+    int res2 = 0;
+    int max = 0;
+
+		line = sc.nextLine();	
+		int n = Integer.parseInt(line);
+		int[] poto = new int[n];
+
+		int f=0;
+		while(sc.hasNextLine()){
+			line = sc.nextLine();	
+			poto[f] = Integer.parseInt(line);			
+			f++;
+		}
+		for(int i =0; i <n; i++){
+			int taille = poto[i];
+			for(int j = i+1; j<n; j++){
+				if(poto[j] >taille){
+					break;
+				}
+				if(poto[j] == taille){
+						res += (j-i);
+					break;
+				}
+			}
+		}
+    System.out.println(res + res2);
+  }
+}
