8fa296a90a15faffda413f1ceffabfb6a2d2d305
Lucas Cimon <lucas.cimon@gmail.com>
2017-02-22 18:29:35 +0100

    Code clean-up in graphviz_mindmap.py

--- languages/python/graphviz_mindmap.py
+++ languages/python/graphviz_mindmap.py
@@ -21,9 +21,6 @@
 import argparse, pydot, shutil, subprocess, sys
 
 
-ROOT_DEFAULT_NAME = '<[root]>'
-
-
 def main(argv):
     print('Using command:', subprocess.check_output([shutil.which('twopi'), '-V'], stderr=subprocess.STDOUT).decode('utf8'), end='', file=sys.stderr)
     args = parse_args(argv)
@@ -46,7 +43,7 @@ def parse_args(argv):
     return parser.parse_args(argv)
 
 
-def create_solarized_mindmap_from_file(input_filepath, layout='twopi', font='arial', gen_dot_file=True, root_label=None):
+def create_solarized_mindmap_from_file(input_filepath, layout='twopi', font='arial', gen_dot_file=False, root_label=None):
     with open(input_filepath) as txt_file:
         text = txt_file.read()
     outfile_basename = input_filepath.rsplit('.', 1)[0]
@@ -60,7 +57,7 @@ def parse_graph(text, root_label=None):
         raise ValueError('Empty graph')
     if text[-1] == '\n':
         text = text[:-1]
-    root = GraphNode(root_label or ROOT_DEFAULT_NAME)
+    root = GraphNode.create_root(root_label)
     last_nodes_per_depth = {0: root}
     last_node = None
     for i, line in enumerate(text.splitlines()):
@@ -81,41 +78,50 @@ def parse_graph(text, root_label=None):
         parent_node = last_nodes_per_depth[depth - 1]
         last_node = parent_node.add_child(value)
         last_nodes_per_depth[last_node.depth] = last_node
-    return root
-
+    return root.set_single_child_as_root()
 
 class GraphNode:
-    last_branch_id = -1
-    known_contents = set()
+    ROOT_DEFAULT_NAME = '<[root]>'
 
     @classmethod
-    def reset(cls):  # for tests
-        cls.last_branch_id = -1
-        cls.known_contents = set()
+    def create_root(cls, content):
+        return cls(content or cls.ROOT_DEFAULT_NAME, parent=None, branch_id=0, known_contents=set())
 
-    def __init__(self, content=None, parent=None):
+    def __init__(self, content, parent, branch_id=None, known_contents=None):
+        self.content = content
+        self.parent = parent
+        self.children = []
+        self._branch_id = branch_id
+        self._last_branch_id = branch_id
+        self._known_contents = known_contents
+
+    def add_child(self, content):
         while content and content in self.known_contents:
             print('Duplicate content found: {}. Using workaround'.format(content), file=sys.stderr)
             content = content + ' '
-        if ':' in content:
-            content = '"{}"'.format(content) # avoid pydot 'port' dectection
         self.known_contents.add(content)
-        self.content = content
-        self.parent = parent
-        self._branch_id = None
-        self.children = []
-
-    def add_child(self, content):
-        child = self.__class__(content, self)
+        branch_id = None if self.parent else self.incr_last_branch_id()
+        child = self.__class__(content, parent=self, branch_id=branch_id)
         self.children.append(child)
         return child
 
-    def set_branch_ids(self):
-        self.last_branch_id += 1
-        self._branch_id = self.last_branch_id
+    def set_single_child_as_root(self):
+        assert not self.parent, 'This should only be called on a graph root'
+        if self.content != self.ROOT_DEFAULT_NAME or len(self.children) != 1:
+            return self
+        old_root = self
+        new_root = old_root.children[0]
+        new_root._known_contents = old_root._known_contents
+        new_root.parent = None
+        new_root._reset_branch_ids()
+        return new_root
+
+    def _reset_branch_ids(self):
+        assert not self.parent, 'This should only be called on a graph root'
+        self._branch_id = 0
+        self._last_branch_id = 0
         for child in self.children:
-            self.last_branch_id += 1
-            child._branch_id = self.last_branch_id
+            child._branch_id = self.incr_last_branch_id()
 
     @property
     def branch_id(self):
@@ -123,6 +129,13 @@ class GraphNode:
         while node._branch_id is None and node.parent:
             node = node.parent
         return node._branch_id
+
+    @property
+    def known_contents(self):
+        node = self
+        while node._known_contents is None and node.parent:
+            node = node.parent
+        return node._known_contents
 
     @property
     def depth(self):
@@ -139,15 +152,17 @@ class GraphNode:
             return 1
         return 1 + max(child.height for child in self.children)
 
-    @property
-    def is_pre_leaf(self):
-        if not self.children:
-            return False
-        return all(not child.children for child in self.children)
+    def incr_last_branch_id(self):
+        node = self
+        while node._last_branch_id is None and node.parent:
+            node = node.parent
+        node._last_branch_id += 1
+        return node._last_branch_id
 
     def __str__(self, indent=''):
-        out = indent + self.content + '\n' if self.parent else ''
-        if self.parent:
+        should_indent = self.content and self.content != self.ROOT_DEFAULT_NAME
+        out = indent + self.content + '\n' if should_indent else ''
+        if should_indent:
             indent += '    '
         for child in self.children:
             out += child.__str__(indent)
@@ -159,17 +174,15 @@ class GraphNode:
             yield from child
 
 
-def create_mindmap(graph, outfile_basename, theme, gen_dot_file):
-    if graph.content == ROOT_DEFAULT_NAME and len(graph.children) == 1:
-        graph = graph.children[0]
-        graph.parent = None
-    graph.set_branch_ids()
+def create_mindmap(graph, outfile_basename, theme, gen_dot_file=False):
     graph_height = graph.height
     g = pydot.Dot(root=graph.content, **theme.graph_style)
     for node in graph:
-        g.add_node(pydot.Node(node.content, **theme.node_style(node.content, node.depth, node.branch_id, graph_height)))
+        content = node.content if ':' not in node.content else '"{}"'.format(node.content) # avoid erroneous pydot 'port' detection
+        g.add_node(pydot.Node(content, **theme.node_style(node, graph_height)))
         if node.parent:
-            g.add_edge(pydot.Edge(node.parent.content, node.content, **theme.edge_style(node.depth, node.branch_id, graph_height)))
+            parent_content = node.parent.content if ':' not in node.parent.content else '"{}"'.format(node.parent.content)
+            g.add_edge(pydot.Edge(parent_content, content, **theme.edge_style(node, graph_height)))
     if gen_dot_file:
         dot_outfile = '{}.dot'.format(outfile_basename)
         print('Generating', dot_outfile, file=sys.stderr)
@@ -203,21 +216,21 @@ class DarkSolarizedTheme:
             bgcolor = self.DARKGREYBLUE,
         )
 
-    def edge_style(self, src_depth, branch_id, graph_height):
+    def edge_style(self, src_node, graph_height):
         return dict(
-            color=self.EDGE_COLORS[branch_id % len(self.EDGE_COLORS)],
+            color=self.EDGE_COLORS[src_node.branch_id % len(self.EDGE_COLORS)],
             dir='none',
-            penwidth=2 * (2 + graph_height - src_depth),
+            penwidth=2 * (2 + graph_height - src_node.depth),
         )
 
-    def node_style(self, content, depth, branch_id, graph_height):
-        label = content.strip() if content and content != ROOT_DEFAULT_NAME else ''
+    def node_style(self, node, graph_height):
+        label = node.content.strip() if node.content and node.content != node.ROOT_DEFAULT_NAME else ''
         return dict(
-            group=branch_id,
+            group=node.branch_id,
             shape='plaintext',
             label=label,
             fontcolor='white',
-            fontsize=2 * (16 + graph_height - depth),
+            fontsize=2 * (16 + graph_height - node.depth),
             fontname=self.graph_style['fontname'], # not inherited by default
         )
 
@@ -240,35 +253,28 @@ def self_test(input_filepath):
     assert len(graph_root.children) == 2
     assert len(graph_root.children[0].children) == 2
     assert graph_root.children[0].children[0].depth == 2
-    GraphNode.reset()
     parse_graph(text + '\n')
-    GraphNode.reset()
     parse_graph('A\n    1\nB    A')
-    GraphNode.reset()
     try:
         parse_graph('')
         assert False
     except ValueError:
         pass
-    GraphNode.reset()
     try:
         parse_graph('\n\n')
         assert False
     except ValueError:
         pass
-    GraphNode.reset()
     try:
         parse_graph('    A')
         assert False
     except ValueError:
         pass
-    GraphNode.reset()
     try:
         parse_graph('A\n  B')
         assert False
     except ValueError:
         pass
-    GraphNode.reset()
     try:
         parse_graph('A\n        B')
         assert False
