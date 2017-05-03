078052c7f20b9bb92fb9877b6c9ab4990ab0b805
Lucas Cimon <lucas.cimon@gmail.com>
2017-05-03 13:32:23 +0200

    Also using requests .elapsed in gevent version timings stats

--- languages/python/crawl_dead_links_gevent.py
+++ languages/python/crawl_dead_links_gevent.py
@@ -6,11 +6,11 @@
 # STDIN FORMAT: 1 URL per line
 # STDOUT FORMAT: [HTTP status | Python exception] URL (for all non-OKs URLs)
 # Note: I had to edit /etc/security/limits.conf in order to increase the nofile soft & hard limits for the user executing this script
-from gevent import monkey, statistics, sleep
+from gevent import monkey, sleep
 from gevent.pool import Pool
 from greenlet import greenlet
 monkey.patch_all(thread=False, select=False)
-import json, sys
+import json, statistics, sys
 from collections import defaultdict
 from datetime import datetime
 from requests import Session
@@ -30,9 +30,9 @@ class PerHostAsyncRequests: # inspired by grequests
             start = perf_counter()
             try:
                 response = self.session.get(url, verify=False)
-                resps.append((url, response.status_code, perf_counter() - start))
+                resps.append((url, response.status_code, perf_counter() - start, response.elapsed.total_seconds()))
             except Exception as error:
-                resps.append((url, error, perf_counter() - start))
+                resps.append((url, error, perf_counter() - start, None))
         return resps
 
 def url_checker(urls):
@@ -44,8 +44,8 @@ def url_checker(urls):
     reqs = (PerHostAsyncRequests(urls) for urls in urls_per_host.values())
     pool = Pool(size=None)
     for resps in pool.imap_unordered(lambda r: r.send(), reqs):
-        for url, status_or_error, exec_duration in resps:
-            yield url, status_or_error, exec_duration, len(pool)
+        for resp in resps:
+            yield resp + (len(pool),)
     pool.join(raise_error=True)
 
 def compute_timing_stats(timings_in_ms):
@@ -78,18 +78,22 @@ if __name__ == '__main__':
     urls = [url.strip() for url in sys.stdin.readlines()]
     start = datetime.utcnow()
     count = 0
-    timings = {}
-    for url, status_or_error, exec_duration, pool_length in url_checker(urls):
-        timings[url] = exec_duration
+    perf_timings, elasped_timings = {}, {}
+    for url, status_or_error, exec_duration, pool_length, elasped_req_time in url_checker(urls):
+        perf_timings[url] = exec_duration
+        if elasped_req_time != None:
+            elasped_timings[url] = elasped_req_time
         count += 1
         # Looks like the following print statements do not get flushed to stdout before the end
         if status_or_error != 200:
             print(status_or_error, url) # this won't be displayed if there are too few URLs (too fast ?)
         if count % (len(urls) // 10) == 0:
-            print('#> {:.1f}% processed : count={} len(pool)={}'.format(count * 100.0 / len(urls), pool_length), file=sys.stderr)
+            print('#> {:.1f}% processed : count={} len(pool)={}'.format(count * 100.0 / len(urls), count, pool_length), file=sys.stderr)
     end = datetime.utcnow()
     print('#= Done in', end - start, file=sys.stderr)
-    print(json.dumps(compute_timing_stats(timings.values()), indent=4), file=sys.stderr)
-    print('Top10 slow requests:', file=sys.stderr)
-    top_slow_urls = sorted(timings.keys(), key=timings.get)[-10:]
-    print('\n'.join('- {} : {:.2f}s'.format(url, timings[url]) for url in top_slow_urls), file=sys.stderr)
+    for name, timings in (('perf', perf_timings), ('requests', elasped_timings)):
+        print('# {} timing stats:'.format(name), file=sys.stderr)
+        print(json.dumps(compute_timing_stats(timings.values()), indent=4), file=sys.stderr)
+        print('## Top10 slow requests:', file=sys.stderr)
+        top_slow_urls = sorted(timings.keys(), key=timings.get)[-10:]
+        print('\n'.join('- {} : {:.2f}s'.format(url, timings[url]) for url in top_slow_urls), file=sys.stderr)
