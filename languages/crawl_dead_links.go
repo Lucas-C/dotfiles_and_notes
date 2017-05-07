df4ea8976de05abc0b12e81b89f9ac538333342a
Lucas Cimon <lucas.cimon@gmail.com>
2017-05-07 23:27:35 +0200

    Adding bucketting per hostname

--- languages/crawl_dead_links.go
+++ languages/crawl_dead_links.go
@@ -1,4 +1,3 @@
-// TODO: bucket URLs per hostname
 // On the complexity of Go timeouts : https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts/
 package main
 
@@ -9,6 +8,7 @@ import (
     "log"
     "math/rand"
     "net/http"
+    "net/url"
     "os"
     "sort"
     "time"
@@ -62,6 +62,20 @@ func percentile(durations []time.Duration, percent float64) float64 {
     return durations[int(index)].Seconds()
 }
 
+func bucketUrlsPerHostname(urls []string) map[string][]string {
+    urlsPerHostnames := make(map[string][]string)
+    for _, hostUrl := range urls {
+        parsedUrl, _ := url.Parse(hostUrls)
+        hostname := parsedUrl.Host
+        if hostUrls, exist := urlsPerHostnames[hostname]; exist {
+            urlsPerHostnames[hostname] = append(hostUrls, hostUrl)
+        } else {
+            urlsPerHostnames[hostname] = []string{hostUrl}
+        }
+    }
+    return urlsPerHostnames
+}
+
 func main() {
     var urls []string
     lineReader := bufio.NewScanner(os.Stdin)
@@ -69,30 +83,37 @@ func main() {
         urls = append(urls, lineReader.Text())
     }
     shuffle(urls)
+    urlsPerHostnames := bucketUrlsPerHostname(urls)
     tr := &http.Transport{
         TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
     }
     client := &http.Client{Transport: tr, Timeout: 120 * time.Second} // there is no timeout by default
     channel := make(chan *CrawlResult /*buffer_size=*/, 100)
     durations := make([]time.Duration, len(urls))
-    for _, url := range urls {
-        go func(url string) {
-            start := time.Now()
-            resp, err := client.Get(url)
-            var result *CrawlResult
-            if resp == nil {
-                result = &CrawlResult{url, 0, err, time.Now().Sub(start)}
-            } else {
-                resp.Body.Close()
-                result = &CrawlResult{url, resp.StatusCode, err, time.Now().Sub(start)}
-            }
-            for { // as long as the channel is full, we retry
-                select {
-                case channel <- result:
-                    return
+    for _, urls := range urlsPerHostnames {
+        go func(urls []string) {
+            for i, url := range urls {
+                if i > 0 {
+                    time.Sleep(2)
+                }
+                start := time.Now()
+                resp, err := client.Get(url)
+                var result *CrawlResult
+                if resp == nil {
+                    result = &CrawlResult{url, 0, err, time.Now().Sub(start)}
+                } else {
+                    resp.Body.Close()
+                    result = &CrawlResult{url, resp.StatusCode, err, time.Now().Sub(start)}
+                }
+                loop:
+                for { // as long as the channel is full, we retry
+                    select {
+                    case channel <- result:
+                        break loop
+                    }
                 }
             }
-        }(url)
+        }(urls)
     }
     count := 0
     for {
