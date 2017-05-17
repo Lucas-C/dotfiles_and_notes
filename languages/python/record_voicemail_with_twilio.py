5a867008f350551e8deab0ebdecd0554a5fe7219
Lucas Cimon <lucas.cimon@gmail.com>
2017-05-17 18:36:54 +0200

    Update record_voicemail_with_twilio.py

--- languages/python/record_voicemail_with_twilio.py
+++ languages/python/record_voicemail_with_twilio.py
@@ -6,17 +6,13 @@ from twilio.rest import Client
 #   export $(xargs < .twilio)
 #   ./record_voicemail_with_twilio.py $phone_number
 
-def main(argv):
-    client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
-    client.calls.create(to=sys.argv[1],
-                        from_=client.incoming_phone_numbers.list()[0].phone_number,
-                        record=True,
-                        url='https://handler.twilio.com/twiml/EH9515e9e0d2fb81f27d75a493225ae703')
+client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
+client.calls.create(to=sys.argv[1],
+                    from_=client.incoming_phone_numbers.list()[0].phone_number,
+                    record=True,
+                    url='https://handler.twilio.com/twiml/EH9515e9e0d2fb81f27d75a493225ae703')
 # This Twiml file content:
 #<?xml version="1.0" encoding="UTF-8"?>
 #<Response>
 #    <Pause length="30"/>
 #</Response>
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
